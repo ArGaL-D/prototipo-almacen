d12af9dc6cd109d95ea659031f8a786f3c9bd4ae
Arturo Galván <arturo.galvan.dmgz@gmail.com>
2021-04-16 13:34:43 -0500

    .

--- frontend/src/components/module/Reparacion.jsx
+++ frontend/src/components/module/Reparacion.jsx
@@ -1,7 +1,8 @@
 import React,{useEffect} from 'react';
 
 export default function Reparacion({setTitle}) {
-
+    
+    //Establecer título actual - navbar
     useEffect(() => {
         setTitle('Reparación');
         sessionStorage.setItem('page','reparacion');
--- frontend/src/components/module/Ubicacion.jsx
+++ frontend/src/components/module/Ubicacion.jsx
@@ -1,15 +1,63 @@
 import React,{useEffect} from 'react';
+import InputDark from '../field/InputDark';
+import Select from '../field/Select';
+import "./styles/Ubicacion.css";
+
+import * as GoIcons from "react-icons/go";
+import Datatable from '../Datatable';
 
 export default function Ubicacion({setTitle}) {
 
+    const columnasUbicacion = [
+        "EQUIPO", "SERIAL", "EDIFICIO",
+        "PISO","AULA","FECHA DE SALIDA"
+    ];
+
+
+    //Establecer título actual - navbar
     useEffect(() => {
         setTitle('Ubicación');
         sessionStorage.setItem('page','ubicacion');
     })
 
     return (
-        <div>
-            UBICACION
+        <div className="module-ubicacion">
+            <div className="container1">
+                
+            </div>
+            
+            <div className="container2">
+                <div className="buscador">
+                    <InputDark
+                        icon = {<GoIcons.GoSearch/>}
+                        placeholder = "buscar"
+                    />
+                </div>
+
+                <div className="ubicacion">
+                    <Select
+                        type = "EDIFICIO"
+                    />
+                    <Select
+                        type = "PISO"
+                    />
+                    <Select
+                        type = "AULA"
+                    />                
+                </div>
+
+                <div className="tabla">
+                    <Datatable
+                        columns = {columnasUbicacion}
+                        rows = {null}
+                    />
+                </div>
+            </div>
+
+
+
+
+
         </div>
     )
 }
