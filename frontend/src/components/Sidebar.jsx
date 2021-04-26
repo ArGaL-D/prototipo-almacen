a1ef961c449406a7db3caff4debfa80769677707
Arturo Galván <arturo.galvan.dmgz@gmail.com>
2021-04-26 12:56:23 -0500

    .

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { Link,useRouteMatch } from 'react-router-dom';
 import { routes } from "../components/config/sidebarData";
 import Tooltip from "./Tooltip";
@@ -6,15 +6,48 @@ import Tooltip from "./Tooltip";
 import "./styles/Sidebar.css";
 
 export default function Sidebar(props) {
+
     let { url } = useRouteMatch();
+
+
+    useEffect(() => {
+        const sidebar = document.getElementById('sidebar');
+        const li_tags = sidebar.childNodes;
+        
+        // Pintar tag (a) al dar click
+        const customizeTagA = (e) => {
+            const tag_a = e.currentTarget;
+            
+
+            li_tags.forEach( li => {
+                if (li.firstChild === tag_a){
+                    tag_a.style.background = 'rgb(250, 210, 135)';
+                    tag_a.firstChild.style.color = '#20232a';
+                }else{
+                    li.firstChild.style.background = '';
+                    li.firstChild.firstChild.style.color = '';
+                }
+            })
+        }
+
+
+        li_tags.forEach( li => {
+            const tag_a = li.firstChild;
+            tag_a.addEventListener('click',customizeTagA);
+        });
+
+
+    },[]);
+
+    
 
     return (
         <div className={props.showSidebar ? "sidebar" : "sidebar active"}>
-           <ul>
+           <ul id="sidebar">
                {
                    routes.map( (route,index) =>{
                     return(
-                        <li key={index}>
+                        <li key={index} className={`li_${index}`}>
                             <Link to={`${url}${route.path}`}>
                                 {route.icon}
                             </Link>
