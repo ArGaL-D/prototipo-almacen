7abf64e21d370ed43ab06c7a6996def8dccf176c
Arturo Galván <arturo.galvan.dmgz@gmail.com>
2021-04-28 11:53:31 -0500

    .

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,59 +1,88 @@
 
+import { useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
+import axios from "axios";
+
+import image1 from "../imgs/img01.jpg";
+import image2 from "../imgs/img02.jpg";
+import image3 from "../imgs/img03.jpg";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import image1 from "../imgs/background.jpg";
-import image2 from "../imgs/edif02.jpg";
-import image3 from "../imgs/edif03.jpg";
-
 export default function ControlledCarousel() {
-    
+  
+  const [images,setImages] = useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/imagenes')
+        .then(resp => {
+            setImages(resp.data);                
+        })
+  },[]);
+
     return (
-        <Carousel>
-
-          <Carousel.Item interval={1000}>
-            <img
-              className="d-block w-100"
-              src={image1}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              <br/>
-              <br/>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item interval={500}>
-            <img
-              className="d-block w-100"
-              src={image1}
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              <br/>
-              <br/>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={image1}
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              <br/>
-              <br/>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-      </Carousel>
+      <>
+        {
+            images.length === 0 
+            ? 
+            <Carousel>
+                <Carousel.Item interval={3000}>
+                    <img
+                      className="d-block w-100"
+                      src={image1}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <h3>Hola</h3>
+                      <br/>
+                      <p>Bienvenido</p>
+                      <br/>                      
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                      className="d-block w-100"
+                      src={image2}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <h3>Hola</h3>
+                      <br/>
+                      <p>Bienvenido</p>
+                      <br/>                    
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                      className="d-block w-100"
+                      src={image3}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <h3>Hola</h3>
+                      <br/>
+                      <p>Bienvenido</p>
+                      <br/>                      
+                    </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            : 
+            <Carousel>
+              {
+                images.map((image,index) => {
+                  return(
+                    <Carousel.Item interval={8000} key={index}>   
+                      <img                        
+                          src = {`http://localhost:3001/${image}`}
+                          alt = "First slide"
+                          className = "d-block w-100"
+                        />
+                    </Carousel.Item>     
+                  )
+                })
+              }
+            </Carousel>
+        }
+    </>
     );
   }
