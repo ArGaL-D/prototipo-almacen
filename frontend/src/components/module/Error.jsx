b8b213c00a7e2f7eac3caddbb1ea831648699d36
Arturo Galván <arturo.galvan.dmgz@gmail.com>
2021-04-28 18:07:05 -0500

    .

--- frontend/src/components/module/Error.jsx
+++ frontend/src/components/module/Error.jsx
@@ -1,9 +1,21 @@
 import {withRouter} from 'react-router-dom'
 
+import "./styles/Error.css";
+
 function Error() {
     return (
-        <div>
-            ERROR
+        <div className="error_container">
+            <div className="error_404">
+                <span>404</span>
+            </div>
+            <div className="text_error">
+                <span>No se econtró la página.</span>
+            </div>
+            <div className="btn_error">
+                <button id="btn_regresar">
+                    REGRESAR
+                </button>
+            </div>
         </div>
     )
 }
