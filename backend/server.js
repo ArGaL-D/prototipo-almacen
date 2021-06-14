dde51f0cc7eacb3b7b99960aa0f164dd9c8c245d
argal <arturo.galvan.dmgz@gmail.com>
2021-06-14 16:02:48 -0500

    .

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ server.set('port', 3001);
 const config_db = {
     host: 'localhost',
     user: 'root',
-    password: 'archDB_32109',
+    password: 'archDB',
     database: 'EQUIPOS_CECYT16',
     multipleStatements: true
 }
